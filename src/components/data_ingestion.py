ca7ce033c78d6e5ffe3a6da9d2bb60f717a0478e
Subhash Dixit <subhashdixit17@gmail.com>
2023-03-12 02:26:29 +0530

    added data tranformationa and utils code

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,10 +8,12 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass #Use to created class variable
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformerConfig
 
 @dataclass
 # To define a class we use __init__. But with dataclass we can directly define class variable
-class DataIngestionConfig:
+class DataIngestionConfig: #Input for data ingestion
     # Any required input for dataingestion
     train_data_path:str = os.path.join("artifacts","train.csv")
     test_data_path:str = os.path.join("artifacts","test.csv")
@@ -50,4 +52,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
--- src/utils.py
+++ src/utils.py
@@ -2,3 +2,24 @@
 # Read a data from database
 # Save a model into cloud 
 # Whenever requirement is there we write function here
+
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import dill
+
+from src.logger import logging
+from src.exception import CustomException
+
+def save_object(file_path, obj):    
+    try:
+        dir_path = os.path.dirname(file_path)
+        # Create directory
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            # Library which will help to create pickle file
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
